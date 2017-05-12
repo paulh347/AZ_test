19e39e0bf2c0a188e0fc5b2200beb32a85a4f3b2
paulh347 <paul.hare@outlook.com>
2017-05-12 13:47:58 +0100

    Update runbookthing.ps1

--- runbooks/runbookthing.ps1
+++ runbooks/runbookthing.ps1
@@ -1,3 +1,9 @@
 #this is line 1
 #this is line 2
 #this is line 3
+workflow Print-Message
+{
+    Write-output "Testing Source Control 001"
+    Write-output "Testing Line two added"
+    Write-output "Testing Line3 added"
+}
